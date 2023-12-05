f96f8a34b8e9ee92099627e4515876e9b7423371
kevin <kevindbaik@gmail.com>
2023-12-05 15:47:53 -0800

    widening and shortening conversions

--- Mod1/Variables.java
+++ Mod1/Variables.java
@@ -6,19 +6,47 @@ public class Variables { // a class
     int petCount = dogCount + catCount;
     System.out.println(petCount);
 
-    byte b = 56;
-    short s = 1024;
-    int i = 314;
-    long lng = -256;
+    byte by = 56;
+    short sh = 1024;
+    int in = 314;
+    long lngg = -256;
 
     /* The following assignments fail.
-    b = 1024;       // value is too big for a byte
-    s = 75000;      // too big for a short
-    i = 3000000000; // too big for an int
+    by = 1024;       // value is too big for a byte
+    sh = 75000;      // too big for a short
+    in = 3000000000; // too big for an int
     */
 
-    float f = 3.14f;
-    double d = 3.14;
+    // whole number types
+    byte b = 8;
+    short s = 16;
+    int i = 32;   // int literal
+    long lng = 64L; // long literal
+
+    // widening conversions (allowed)
+    s = b;    // a byte fits in a short
+    i = s;    // a short fits in an int
+    lng = i;  // an int fits in a long
+    lng = 32; // `32` is an integer literal, it fits in a long
+
+    // narrowing conversions (not allowed)
+    // b = s;   // compiler error: cannot convert from short to byte
+    // s = i;   // compiler error: cannot convert from int to short
+    // i = lng; // compiler error: cannot convert from long to int
+
+    // floating-point types
+    float f = 32.0f; // float literal
+    double d = 64.0; // double literal
+
+    // widening conversation (allowed)
+    d = f;   // a float fits in a double
+    f = i;   // an int fits in a float
+    d = lng; // a long fits in a double
+
+    // narrowing conversions (not allowed)
+    // i = f;   // compiler error: cannot convert from float to int
+    // f = d;   // compiler error: cannot convert from double to float
+    // lng = d; // compiler error: cannot convert from double to long
   }
 
 }
